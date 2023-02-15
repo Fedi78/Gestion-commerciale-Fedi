340fbf1d9985ab9b9798bd61e1f64895781e9a40
Fedi78 <fedidou78@gmail.com>
2023-02-15 15:43:14 +0100

    commit

--- gestion commerciale Fedi/Program.cs
+++ gestion commerciale Fedi/Program.cs
@@ -1,22 +1,2 @@
 ﻿// EL Manoubi Fedi
 using System;
-
-
-namespace gestion_commerciale_Fedi
-{
-    class Commerciale
-    {
-      private string nom;
-      private string prenom;
-      private int anneeNaissance;
-
-        public string Nom { get => nom; set => nom = value; }
-        public string Prenom { get => prenom; set => prenom = value; }
-        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
-    }
-
-
-
-
-
-}
---
+++ gestion commerciale Fedi/commercial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_commerciale_Fedi
+
+{
+    class Commercial
+    {
+        string prenom; 
+        int anneeNaissance;
+        string nom;
+       
+
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
+
+        public Commercial(string pprenom, string pnom, int panneeNaissance)
+        {
+            this.Prenom = pprenom;
+            this.Nom = pnom;
+            this.AnneeNaissance = panneeNaissance;
+        }
+        public Commercial()
+        {
+
+        }
+        public int calculAge()
+        {
+            return DateTime.Now.Year - this.AnneeNaissance;
+        }
+        public override string ToString()
+        {
+
+        }
+
+    }
+}
+
+   
