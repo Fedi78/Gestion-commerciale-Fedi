17c4268d0fa36851818818cafe8c104b311398e1
Fedi78 <fedidou78@gmail.com>
2023-02-15 14:51:41 +0100

    commit du jour

--- gestion commerciale Fedi/Program.cs
+++ gestion commerciale Fedi/Program.cs
@@ -1,3 +1,22 @@
-﻿// EL Manoubi Fédi
+﻿// EL Manoubi Fedi
+using System;
 
-test
+
+namespace gestion_commerciale_Fedi
+{
+    class Commerciale
+    {
+      private string nom;
+      private string prenom;
+      private int anneeNaissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
+    }
+
+
+
+
+
+}
