f76f1d1f1f064ddaddb15976ee246581e75e6c33
Fedi78 <fedidou78@gmail.com>
2023-02-16 12:10:30 +0100

    commit 4

---
+++ gestion commerciale Fedi/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_commerciale_Fedi
+{
+    internal class Service
+    {
+        private string NomDuService
+    }
+}
